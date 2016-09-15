ae34f6664f4c4a40171d772002db51c2b4c8e187
gosborn <gosborn@tonicdesign.com>
2016-09-15 05:49:32 -0400

    Refactors invoice_generator to use job, changes order of methods

--- app/models/invoice_generator.rb
+++ app/models/invoice_generator.rb
@@ -3,18 +3,20 @@ require 'action_view'
 class InvoiceGenerator
   include ActionView::Helpers::NumberHelper
 
+  attr_reader :job, :start_date, :end_date
+
   def initialize(args)
     @start_date = args[:start_date]
     @end_date = args[:end_date]
-    @job = Job.find(args[:job_id])
+    @job = args[:job]
   end
 
   def invoice
     {
-      job: @job.title,
+      job: job.title,
       total_minutes: total_minutes,
-      hourly_rate: "$#{number_with_precision(@job.hourly_rate, precision: 2)}",
-      tax_rate: @job.tax_rate,
+      hourly_rate: "$#{number_with_precision(job.hourly_rate, precision: 2)}",
+      tax_rate: job.tax_rate,
       date_range: date_range,
       sub_total: money_format(sub_total),
       tax: money_format(tax),
@@ -23,14 +25,42 @@ class InvoiceGenerator
     }
   end
 
-  def time_entries
-    @time_entries ||= @job.time_entries.where(date: Date.parse(@start_date)..Date.parse(@end_date))
+  private
+
+  def total_minutes
+    time_entries.inject(0) { |sum, time_entry| sum + time_entry.time_spent }  
   end
 
-  private
+  def time_entries
+    @time_entries ||= job.time_entries.where(date: Date.parse(@start_date)..Date.parse(@end_date))
+  end
 
   def date_range
     "#{Date.parse(@start_date).strftime("%B %d, %Y")} - #{Date.parse(@end_date).strftime("%B %d, %Y")}"
+  end
+
+  def money_format(amount)
+    "$#{number_with_precision(amount/100, precision: 2)}"
+  end
+
+  def sub_total
+    job_rate * total_minutes/60.to_f
+  end
+
+  def job_rate
+    job.hourly_rate * 100
+  end
+
+  def tax
+    tax_rate * sub_total
+  end
+
+  def tax_rate
+    job.tax_rate / 100
+  end
+
+   def total
+    sub_total + tax
   end
 
   def formatted_time_entries
@@ -41,35 +71,5 @@ class InvoiceGenerator
         time_spent: time_entry.time_spent
       }
     end
-  end
-
-  def total_minutes
-    time_entries.inject(0) { |sum, time_entry| sum + time_entry.time_spent }  
-  end
-
-  def job_rate
-    # dollars per hour
-    @job.hourly_rate * 1000
-  end
-
-  def tax_rate
-    # percentage
-    @job.tax_rate / 100
-  end
-
-  def sub_total
-    job_rate * total_minutes/60.to_f
-  end
-
-  def money_format(amount)
-    "$#{number_with_precision(amount/1000, precision: 2)}"
-  end
-
-  def tax
-    tax_rate * sub_total
-  end
-
-  def total
-    sub_total + tax
   end
 end
