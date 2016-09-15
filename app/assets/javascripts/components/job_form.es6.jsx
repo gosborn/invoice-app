baa18a65b5b7d53bf95f732ce0bf17f0dffab187
gosborn <gosborn@tonicdesign.com>
2016-09-14 22:41:49 -0400

    Cleans up job_form by following airbnb style guidelines

--- app/assets/javascripts/components/job_form.es6.jsx
+++ app/assets/javascripts/components/job_form.es6.jsx
@@ -7,58 +7,30 @@ class JobForm extends React.Component {
   }
 
   render() {
-    return(
+    return (
       <form className='form' onSubmit={this.handleSubmit}>
         <div className='form-group' id='title_form'>
-          <input type='text' className='form-control'
-                 placeholder='Title' name='title'
-                 value={this.state.title} onChange={this.handleChange}>
-          </input>
+          <input className='form-control' type='text' name='title'
+                 placeholder='Title' value={this.state.title} onChange={this.handleChange} />
         </div>
         <div className='input-group' id='hourly_rate_group'>
-          <span className="input-group-addon" id="basic-addon1">$</span>
-          <input type='number' className='form-control'
-                 placeholder='Hourly rate' name='hourly_rate' step="any"
-                 value={this.state.hourly_rate} min="0" onChange={this.handleChange}>
-          </input>
+          <span className='input-group-addon' id='basic-addon1'>$</span>
+          <input className='form-control' type='number' min='0'
+                 step='any' name='hourly_rate' placeholder='Hourly rate'
+                 value={this.state.hourly_rate}  onChange={this.handleChange} />
         </div>
         <div className='input-group' id='tax_rate_group'>
-          <input type='number' className='form-control'
-                 placeholder='Tax rate' name='tax_rate' step="any"
-                 value={this.state.tax_rate} min="0" onChange={this.handleChange}>
-          </input>
-          <span className="input-group-addon" id="basic-addon2">%</span>
+          <input className='form-control' type='number' min='0'
+                 step='any' name='tax_rate' placeholder='Tax rate'  
+                 value={this.state.tax_rate}  onChange={this.handleChange} />
+          <span className='input-group-addon' id='basic-addon2'>%</span>
         </div>
         <div className='form-group'>
-          <input type='submit' className='btn btn-primary' id='new_job_button'
-                 disabled={!this.valid()} value='Add a new job'>
-          </input>
+          <input className='btn btn-primary' id='new_job_button' type='submit'  
+                 disabled={!this.valid()} value='Add a new job' />
         </div>
       </form>
-    );
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    $.post('/api/v1/jobs',
-      { job: this.state },
-      function(data) {
-        this.props.handleNewRecord(data)
-        this.setState(this.blankState());
-      }.bind(this),
-      'JSON'
-    );
-  }
-
-  handleChange(e) {
-    var name = e.target.name;
-    var obj = {};
-    obj[name] = e.target.value;
-    this.setState(obj);
-  }
-
-  valid() {
-    return (this.state.title && this.state.tax_rate && this.state.hourly_rate);
+    )
   }
 
   blankState() {
@@ -67,5 +39,29 @@ class JobForm extends React.Component {
       tax_rate: '',
       hourly_rate: '',
     }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    $.post('/api/v1/jobs',
+      { job: this.state },
+      data => {
+        this.props.handleNewRecord(data)
+        this.setState(this.blankState())
+      },
+      'JSON'
+    )
+  }
+
+  handleChange(e) {
+    var name = e.target.name,
+        obj = {}
+
+    obj[name] = e.target.value
+    this.setState(obj)
+  }
+
+  valid() {
+    return (this.state.title && this.state.tax_rate && this.state.hourly_rate);
   }
 }
