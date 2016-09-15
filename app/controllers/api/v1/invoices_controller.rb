18766d80b67e489420e4da3505e463d9fcb9b550
gosborn <gosborn@tonicdesign.com>
2016-09-15 04:50:03 -0400

    Refactors the index endpoint for the invoices API. Moves margin to intializer, current_job and invoice to private methods.

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -3,29 +3,29 @@ module Api
     class InvoicesController < ApplicationController
 
       def index
-        @job = current_user.jobs.where(id: invoice_params[:job_id]).take!
-        @invoice = InvoiceGenerator.new({
-          start_date: invoice_params[:start_date],
-          end_date: invoice_params[:end_date],
-          job_id: @job.id
-        })
-
-        render pdf: "#{@job.title}_invoice_#{invoice_params[:start_date]}-#{invoice_params[:end_date]}",
+        render pdf: "#{current_job.title}_invoice_#{invoice_params[:start_date]}-#{invoice_params[:end_date]}",
                template: 'invoices/invoice.html.erb',
                disposition: 'attachment',
                layout: 'pdf',
-               show_as_html: params.key?('debug'),
-               locals: { invoice: @invoice },
-               margin:  {   top:               3,                
-                            bottom:            3,
-                            left:              3,
-                            right:             3}
+               locals: { invoice: invoice }
       end
 
       private
 
       def invoice_params
         params.require(:invoice).permit(:job_id, :start_date, :end_date)
+      end
+
+      def current_job
+        @job ||= current_user.jobs.where(id: invoice_params[:job_id]).take!
+      end
+
+      def invoice
+        InvoiceGenerator.new({
+          start_date: invoice_params[:start_date],
+          end_date: invoice_params[:end_date],
+          job_id: current_job.id
+        }).invoice
       end
     end
   end
