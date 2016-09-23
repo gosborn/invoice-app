7e31905f1b2a5cc34fc9246add20565f7913dc1b
gosborn <gosborn@tonicdesign.com>
2016-09-23 09:49:27 -0400

    Removes setstate warning from react.

--- app/assets/javascripts/components/job_form.es6.jsx
+++ app/assets/javascripts/components/job_form.es6.jsx
@@ -5,7 +5,7 @@ export default class JobForm extends React.Component {
     super()
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleChange = this.handleChange.bind(this)
-    this.state = this.blankState()
+    this.state = {}
   }
 
   render() {
@@ -13,18 +13,18 @@ export default class JobForm extends React.Component {
       <form className='form' onSubmit={this.handleSubmit}>
         <div className='form-group' id='title_form'>
           <input className='form-control' type='text' name='title'
-                 placeholder='Title' value={this.state.title} onChange={this.handleChange} />
+                 placeholder='Title' value={this.state.title || ''} onChange={this.handleChange} />
         </div>
         <div className='input-group' id='hourly_rate_group'>
           <span className='input-group-addon' id='basic-addon1'>$</span>
           <input className='form-control' type='number' min='0'
                  step='any' name='hourly_rate' placeholder='Hourly rate'
-                 value={this.state.hourly_rate}  onChange={this.handleChange} />
+                 value={this.state.hourly_rate || ''}  onChange={this.handleChange} />
         </div>
         <div className='input-group' id='tax_rate_group'>
           <input className='form-control' type='number' min='0'
                  step='any' name='tax_rate' placeholder='Tax rate'  
-                 value={this.state.tax_rate}  onChange={this.handleChange} />
+                 value={this.state.tax_rate || ''}  onChange={this.handleChange} />
           <span className='input-group-addon' id='basic-addon2'>%</span>
         </div>
         <div className='form-group'>
@@ -35,21 +35,12 @@ export default class JobForm extends React.Component {
     )
   }
 
-  blankState() {
-    return {
-      title: '',
-      tax_rate: '',
-      hourly_rate: '',
-    }
-  }
-
   handleSubmit(e) {
     e.preventDefault()
     $.post('/api/v1/jobs',
       { job: this.state },
       data => {
         this.props.handleNewJob(data)
-        this.setState(this.blankState())
       },
       'JSON'
     )
